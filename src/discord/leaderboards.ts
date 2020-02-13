9e24af65672a3c1c26e83a56eda1af9a77bcce22
hpfxd <me@hpfxd.nl>
2020-02-13 09:42:12 -0500

    add leaderboard colors for embeds

--- src/discord/leaderboards.ts
+++ src/discord/leaderboards.ts
@@ -7,11 +7,13 @@ import { config } from "../index";
 
 class Leaderboard {
     name: string;
+    color: number;
     message: Message;
     updater: Function;
 
-    constructor(name: string, message: Message, updater: Function) {
+    constructor(name: string, color: number, message: Message, updater: Function) {
         this.name = name;
+        this.color = color;
         this.message = message;
         this.updater = updater;
     }
@@ -20,6 +22,7 @@ class Leaderboard {
         const embed = new RichEmbed();
         embed.setTitle(this.name);
         embed.setTimestamp(new Date());
+        embed.setColor(this.color);
 
         const result: {
             name: string;
@@ -65,7 +68,7 @@ export default class Leaderboards {
     private async setupLeaderboards() {
         const messages = config["discord"]["leaderboards"]["messages"];
 
-        this.leaderboards.push(new Leaderboard("Lifetime Guild Experience", await this.channel.fetchMessage(messages["lifetimeXp"]), async () => {
+        this.leaderboards.push(new Leaderboard("Lifetime Guild Experience", 0x4293f5, await this.channel.fetchMessage(messages["lifetimeXp"]), async () => {
             const users = await db.sequelize.query("SELECT minecraftId, sum(experience) AS experience FROM experienceHistory GROUP BY minecraftId ORDER BY experience DESC LIMIT 10", {
                 type: QueryTypes.SELECT
             });
@@ -83,7 +86,7 @@ export default class Leaderboards {
             return result;
         }));
 
-        this.leaderboards.push(new Leaderboard("Weekly Guild Experience", await this.channel.fetchMessage(messages["weeklyXp"]), async (guild: hapi.Guild) => {
+        this.leaderboards.push(new Leaderboard("Weekly Guild Experience", 0x5742f5, await this.channel.fetchMessage(messages["weeklyXp"]), async (guild: hapi.Guild) => {
             const res = [];
             for (const member of guild.members) {
                 res.push({
@@ -109,7 +112,7 @@ export default class Leaderboards {
             return result;
         }));
 
-        this.leaderboards.push(new Leaderboard("Daily Guild Experience", await this.channel.fetchMessage(messages["dailyXp"]), async (guild: hapi.Guild) => {
+        this.leaderboards.push(new Leaderboard("Daily Guild Experience", 0xb942f5, await this.channel.fetchMessage(messages["dailyXp"]), async (guild: hapi.Guild) => {
             const res = [];
             for (const member of guild.members) {
                 res.push({
